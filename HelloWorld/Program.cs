4d4651feab11666a8a3664dbe7f99960a64689ad
Vitor <vitorbuon@gmail.com>
2020-08-21 16:09:23 -0300

    Add teste

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,6 +4,7 @@ namespace HelloWorld {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("teste");
         }
     }
 }
